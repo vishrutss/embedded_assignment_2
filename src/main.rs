ae10d75ea60548b09044a0c212780253f6695a81
vishrutss <vishrutss@gmail.com>
2024-02-08 11:24:51 -0800

    Initial code to display dot in the middle of the board

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,28 @@ use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
 
 use cortex_m_rt::entry;
-use microbit::board::Board;
+use microbit::{
+    board::Board,
+    display::blocking::Display,
+    hal::{prelude::*, timer},
+};
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
-    let _board = Board::take().unwrap();
-    let mut counter = 0u64;
+    let board = Board::take().unwrap();
+    let pins = board.display_pins;
+    let mut image = [[0,0,0,0,0],
+                                    [0,0,0,0,0],
+                                    [0,0,1,0,0],
+                                    [0,0,0,0,0],
+                                    [0,0,0,0,0]];
+
+    let mut delay = timer::Timer::new(board.TIMER0);
+
+    let mut display = Display::new(pins);
     loop {
-        rprintln!("{}", counter);
-        counter += 1;
+        rprintln!("showing");
+        display.show(&mut delay, image, 1000);
     }
 }

f821b25d86b351095a71f36dacfd22a9835bf694
vishrutss <vishrutss@gmail.com>
2024-02-10 10:01:20 -0800

    Added make_sound function

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,7 @@
 use cortex_m_rt::entry;
 use lsm303agr::Lsm303agr;
 use microbit::{
-    board::Board,
-    display::blocking::Display,
-    hal::{prelude::*, twim, Timer},
-    pac::twim0::frequency::FREQUENCY_A,
+    board::Board, display::blocking::Display, gpio::SPEAKER, hal::{delay::Delay, gpio::Level, prelude::*, twim, Timer}, pac::twim0::frequency::FREQUENCY_A
 };
 use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
@@ -32,6 +29,13 @@ fn reset_board(image: &mut [[u8; 5]; 5]) {
     ];
 }
 
+fn make_sound(speaker: &mut SPEAKER, delay:&mut Delay) {
+    speaker.set_high().unwrap();
+    delay.delay_us(5000u16);
+    speaker.set_low().unwrap();
+    delay.delay_us(500u16);
+}
+
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
@@ -51,6 +55,9 @@ fn main() -> ! {
         )
         .unwrap();
 
+    let mut delay = Delay::new(board.SYST);
+    let mut speaker = board.speaker_pin.into_push_pull_output(Level::Low);
+
     let pins = board.display_pins;
     let mut image = [[0; 5]; 5];
     reset_board(&mut image);
@@ -59,12 +66,18 @@ fn main() -> ! {
     loop {
         rprintln!("showing");
         display.show(&mut timer, image, 1000);
+        reset_board(&mut image);
         let status = sensor.accel_status().unwrap();
         if status.xyz_new_data() {
             let data = sensor.acceleration().unwrap();
             rprintln!("{:?}", data.x_mg());
             rprintln!("{:?}", data.y_mg());
             rprintln!("{:?}", data.z_mg());
+            if data.x_mg() > 500 {
+                rprintln!("updating");
+                update_board(&mut image);
+                make_sound(&mut speaker, &mut delay);
+            }
         }
     }
 }

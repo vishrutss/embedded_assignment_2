f2eb101f5dcd63142c07ed657bbfa2e69ab38ad7
vishrutss <vishrutss@gmail.com>
2024-02-09 18:35:44 -0800

    Implemented the accelerometer in the LSM303AGR

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,55 @@
 #![no_main]
 #![no_std]
 
-use panic_rtt_target as _;
-use rtt_target::{rprintln, rtt_init_print};
-
 use cortex_m_rt::entry;
+use lsm303agr::Lsm303agr;
 use microbit::{
     board::Board,
     display::blocking::Display,
-    hal::{prelude::*, timer},
+    hal::{prelude::*, twim, Timer},
+    pac::twim0::frequency::FREQUENCY_A,
 };
+use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     let board = Board::take().unwrap();
-    let pins = board.display_pins;
-    let mut image = [[0,0,0,0,0],
-                                    [0,0,0,0,0],
-                                    [0,0,1,0,0],
-                                    [0,0,0,0,0],
-                                    [0,0,0,0,0]];
+    let i2c = twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100);
+    
+    let mut timer = Timer::new(board.TIMER0);
 
-    let mut delay = timer::Timer::new(board.TIMER0);
+    let mut sensor = Lsm303agr::new_with_i2c(i2c);
 
+    sensor.init().unwrap();
+    sensor
+        .set_accel_mode_and_odr(
+            &mut timer,
+            lsm303agr::AccelMode::Normal,
+            lsm303agr::AccelOutputDataRate::Hz10,
+        )
+        .unwrap();
+
+        let pins = board.display_pins;
+        let mut image = [
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+        ];
+    
     let mut display = Display::new(pins);
     loop {
         rprintln!("showing");
-        display.show(&mut delay, image, 1000);
+        display.show(&mut timer, image, 1000);
+        let status = sensor.accel_status().unwrap();
+        if status.xyz_new_data() {
+            let data = sensor.acceleration().unwrap();
+            rprintln!("{:?}", data.x_mg());
+            rprintln!("{:?}", data.y_mg());
+            rprintln!("{:?}", data.z_mg());
+        }
     }
 }

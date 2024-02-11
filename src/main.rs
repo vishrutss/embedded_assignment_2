5245e70282a017adb6abb151446703ee0ec5f11f
vishrutss <vishrutss@gmail.com>
2024-02-11 14:55:14 -0800

    updated acceleration code

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,11 @@
 use cortex_m_rt::entry;
 use lsm303agr::Lsm303agr;
 use microbit::{
-    board::Board, display::blocking::Display, gpio::SPEAKER, hal::{delay::Delay, gpio::Level, prelude::*, twim, Timer}, pac::twim0::frequency::FREQUENCY_A
+    board::Board,
+    display::blocking::Display,
+    gpio::SPEAKER,
+    hal::{delay::Delay, gpio::Level, prelude::*, twim, Timer},
+    pac::twim0::frequency::FREQUENCY_A,
 };
 use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
@@ -29,7 +33,7 @@ fn reset_board(image: &mut [[u8; 5]; 5]) {
     ];
 }
 
-fn make_sound(speaker: &mut SPEAKER, delay:&mut Delay) {
+fn make_sound(speaker: &mut SPEAKER, delay: &mut Delay) {
     speaker.set_high().unwrap();
     delay.delay_us(5000u16);
     speaker.set_low().unwrap();
@@ -57,7 +61,6 @@ fn main() -> ! {
 
     let mut delay = Delay::new(board.SYST);
     let mut speaker = board.speaker_pin.into_push_pull_output(Level::Low);
-
     let pins = board.display_pins;
     let mut image = [[0; 5]; 5];
     reset_board(&mut image);
@@ -70,10 +73,11 @@ fn main() -> ! {
         let status = sensor.accel_status().unwrap();
         if status.xyz_new_data() {
             let data = sensor.acceleration().unwrap();
-            rprintln!("{:?}", data.x_mg());
-            rprintln!("{:?}", data.y_mg());
-            rprintln!("{:?}", data.z_mg());
-            if data.x_mg() > 500 {
+            let x: f64 = (data.x_mg() * 1000).into();
+            let y: f64 = (data.y_mg() * 1000).into();
+            let z: f64 = (data.z_mg() * 1000).into();
+            let magnitude: f64 = (x * x) + (y * y) + (z * z);
+            if magnitude < 0.25 {
                 rprintln!("updating");
                 update_board(&mut image);
                 make_sound(&mut speaker, &mut delay);
